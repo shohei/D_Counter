33dad23d01128e036dbcd72b8c2e6ccb1f017284
Shohei Aoki <shoaok@gmail.com>
2014-09-27 12:21:46 +0900

    Initial Commit

---
+++ Counter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Counter
+//
+//  Created by shohei on 9/27/14.
+//  Copyright (c) 2014 shohei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
